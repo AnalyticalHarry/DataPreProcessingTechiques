3ef471bf3f256609d963fac481258b28dc2d99de
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-02-13 10:24:20 +0000

    Update correlation_heatmap.py

--- correlation_heatmap.py
+++ correlation_heatmap.py
@@ -1,5 +1,5 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#plot heat map even value is categorical 
 (df.select_dtypes(include='number').join(pd.get_dummies(df.select_dtypes(exclude='number')))).corr().pipe((sns.heatmap, 'data'), annot=True, cmap='coolwarm', fmt=".2f") and plt.show()
-plt.savefig('correlation_heatmap.png')
